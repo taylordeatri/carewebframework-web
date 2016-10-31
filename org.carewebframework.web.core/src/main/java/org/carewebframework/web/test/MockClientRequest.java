d3cf04fa518f6cd81f1fd95b2ee0526c656f12d8
Douglas Martin <dkmartin@regenstrief.org>
2016-10-31 10:26:37 -0400

    Add test framework.

---
+++ org.carewebframework.web.core/src/main/java/org/carewebframework/web/test/MockClientRequest.java
@@ -0,0 +1,13 @@
+package org.carewebframework.web.test;
+
+import java.util.Map;
+
+import org.carewebframework.web.client.ClientRequest;
+
+public class MockClientRequest extends ClientRequest {
+    
+    public MockClientRequest(MockSession session, Map<String, Object> map) {
+        super(session, map);
+    }
+    
+}
