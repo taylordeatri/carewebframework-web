e59519b7dfeb7ae723c4f265576a167b0368fe4e
Douglas Martin <dkmartin@regenstrief.org>
2017-02-26 13:20:58 -0500

    Add Spring namespace support for component scanning.

---
+++ org.carewebframework.web.core/src/main/java/org/carewebframework/web/spring/ComponentXmlParser.java
@@ -0,0 +1,112 @@
+/*
+ * #%L
+ * carewebframework
+ * %%
+ * Copyright (C) 2008 - 2016 Regenstrief Institute, Inc.
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * This Source Code Form is also subject to the terms of the Health-Related
+ * Additional Disclaimer of Warranty and Limitation of Liability available at
+ *
+ *      http://www.carewebframework.org/licensing/disclaimer.
+ *
+ * #L%
+ */
+package org.carewebframework.web.spring;
+
+import org.apache.commons.lang.ArrayUtils;
+import org.springframework.beans.factory.support.BeanDefinitionBuilder;
+import org.springframework.beans.factory.xml.AbstractSingleBeanDefinitionParser;
+import org.springframework.expression.ParseException;
+import org.w3c.dom.Attr;
+import org.w3c.dom.Element;
+
+/**
+ * Provides namespace support for scanning for CWF component annotations. For example,
+ *
+ * <pre>
+ * {@code
+ * <beans xmlns="http://www.springframework.org/schema/beans"
+ *    xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+ *    xmlns:cwc="http://www.carewebframework.org/schema/component"
+ *    xsi:schemaLocation="
+ *        http://www.springframework.org/schema/beans
+ *        http://www.springframework.org/schema/beans/spring-beans.xsd
+ *        http://www.carewebframework.org/schema/component
+ *        http://www.carewebframework.org/schema/component/component-extensions.xsd">
+ *
+ *    <!-- Scan by package -->
+ *    <cwc:component package="org.carewebframework.shell" />
+ *    <!-- Scan by class -->
+ *    <cwc:component class="org.carewebframework.shell.CareWebShell" />
+ * </beans>
+ * }
+ * </pre>
+ */
+public class ComponentXmlParser extends AbstractSingleBeanDefinitionParser {
+
+    private static final String[] ATTRIBUTES = { "class", "package" };
+
+    @Override
+    protected String getBeanClassName(Element element) {
+        return "org.springframework.beans.factory.config.MethodInvokingBean";
+    }
+
+    @Override
+    protected void doParse(Element element, BeanDefinitionBuilder builder) {
+        builder.addPropertyReference("targetObject", "cwf_ComponentScanner");
+        builder.addPropertyValue("targetMethod", "scan");
+        
+        if (element.getAttributes().getLength() > 1) {
+            error("Invalid attributes specified");
+        }
+        
+        Attr attr = (Attr) element.getAttributes().item(0);
+        int i = attr == null ? -2 : ArrayUtils.indexOf(ATTRIBUTES, attr.getName());
+        Object arg = null;
+        
+        switch (i) {
+            case -2:
+                error("Missing attribute");
+                
+            case -1:
+                error("Unrecognized attribute: " + attr.getName());
+                
+            case 0: // class
+                try {
+                    arg = Class.forName(attr.getValue());
+                } catch (ClassNotFoundException e) {
+                    throw new ParseException(0, "Error resolving class attribute", e);
+                }
+                
+                break;
+
+            case 1: // package
+                arg = attr.getValue();
+                break;
+
+        }
+
+        builder.addPropertyValue("arguments", new Object[] { arg });
+    }
+    
+    @Override
+    protected boolean shouldGenerateId() {
+        return true;
+    }
+    
+    private void error(String message) {
+        throw new ParseException(0, message + ".\n" + "Must be one of: " + ATTRIBUTES);
+    }
+}
