6a721e219f3df03a4ccfd956a53d493685c11a84
Douglas Martin <dkmartin@regenstrief.org>
2017-02-26 10:16:37 -0500

    Remove throws modifier.

--- org.carewebframework.web.core/src/main/java/org/carewebframework/web/servlet/ServletConfiguration.java
+++ org.carewebframework.web.core/src/main/java/org/carewebframework/web/servlet/ServletConfiguration.java
@@ -7,15 +7,15 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * 
+ *
  * This Source Code Form is also subject to the terms of the Health-Related
  * Additional Disclaimer of Warranty and Limitation of Liability available at
  *
@@ -42,35 +42,35 @@ import org.springframework.web.servlet.resource.VersionResourceResolver;
 @EnableWebMvc
 @Configuration
 public class ServletConfiguration extends WebMvcConfigurerAdapter {
-    
+
     private final GzipResourceResolver gzipResourceResolver = new GzipResourceResolver();
-    
+
     private final VersionResourceResolver versionResourceResolver = new VersionResourceResolver()
             .addContentVersionStrategy("/**");
-    
+
     private final CwfResourceTransformer cwfResourceTransformer = new CwfResourceTransformer();
-    
+
     private final AppCacheManifestTransformer appCacheManifestTransformer = new AppCacheManifestTransformer();
-    
+
     private final MinifiedResourceResolver minifiedResourceResolver = new MinifiedResourceResolver("js", "css");
-    
-    public ServletConfiguration() throws Exception {
+
+    public ServletConfiguration() {
         ComponentScanner.getInstance().scan(BaseComponent.class.getPackage());
         EventTypeScanner.getInstance().scan(Event.class.getPackage());
     }
-    
+
     @Override
     public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
         configurer.mediaType("cwf", MediaType.TEXT_HTML);
     }
-    
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         addResourceHandlers(registry, "/webjars/**", "classpath:/META-INF/resources/webjars/");
         addResourceHandlers(registry, "/web/**", "classpath:/web/");
         addResourceHandlers(registry, "/**", "/");
     }
-    
+
     private void addResourceHandlers(ResourceHandlerRegistry registry, String pattern, String locations) {
         //@formatter:off
         registry
@@ -84,5 +84,5 @@ public class ServletConfiguration extends WebMvcConfigurerAdapter {
             .addTransformer(appCacheManifestTransformer);
         //@formatter:on
     }
-    
+
 }
