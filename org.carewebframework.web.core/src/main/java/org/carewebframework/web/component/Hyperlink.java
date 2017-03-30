8755572ec2e5a9d94f3fc2663b07a6292077271d
Douglas Martin <dkmartin@regenstrief.org>
2017-03-30 09:46:59 -0500

    Rename "a" tag to "link"

--- org.carewebframework.web.core/src/main/java/org/carewebframework/web/component/Hyperlink.java
+++ org.carewebframework.web.core/src/main/java/org/carewebframework/web/component/Hyperlink.java
@@ -7,15 +7,15 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * 
+ *
  * This Source Code Form is also subject to the terms of the Health-Related
  * Additional Disclaimer of Warranty and Limitation of Liability available at
  *
@@ -32,20 +32,20 @@ import org.carewebframework.web.annotation.Component.PropertySetter;
 /**
  * A simple hyperlink (anchor) component.
  */
-@Component(value = "a", widgetClass = "Hyperlink", parentTag = "*")
+@Component(value = "link", widgetClass = "Hyperlink", parentTag = "*")
 public class Hyperlink extends BaseLabeledImageComponent<BaseLabeledComponent.LabelPositionHorz> {
-    
+
     private String href;
-    
+
     private String target;
-    
+
     public Hyperlink() {
         addClass("flavor:btn-link size:btn-sm");
     }
-    
+
     /**
      * Returns the position of the label relative to the contained elements. Defaults to 'left'.
-     * 
+     *
      * @return May be one of: left, right.
      */
     @Override
@@ -53,10 +53,10 @@ public class Hyperlink extends BaseLabeledImageComponent<BaseLabeledComponent.La
     public LabelPositionHorz getPosition() {
         return super.getPosition();
     }
-    
+
     /**
      * Sets the position of the label relative to the contained elements.
-     * 
+     *
      * @param position May be one of: left, right.
      */
     @Override
@@ -64,29 +64,29 @@ public class Hyperlink extends BaseLabeledImageComponent<BaseLabeledComponent.La
     public void setPosition(LabelPositionHorz position) {
         super.setPosition(position);
     }
-    
+
     @PropertyGetter("href")
     public String getHref() {
         return href;
     }
-    
+
     @PropertySetter("href")
     public void setHref(String href) {
         if (!areEqual(href = nullify(href), this.href)) {
             sync("href", this.href = href);
         }
     }
-    
+
     @PropertyGetter("target")
     public String getTarget() {
         return target;
     }
-    
+
     @PropertySetter("target")
     public void setTarget(String target) {
         if (!areEqual(href = nullify(target), this.target)) {
             sync("target", this.target = target);
         }
     }
-    
+
 }
