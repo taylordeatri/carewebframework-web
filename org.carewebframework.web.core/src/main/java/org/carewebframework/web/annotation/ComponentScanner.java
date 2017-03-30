4c5ebb403eb0f4862c015610a23fc82f8807ad1b
Douglas Martin <dkmartin@regenstrief.org>
2017-03-30 09:46:26 -0500

    Component scanner now correctly ignores synthetic and bridge methods.

--- org.carewebframework.web.core/src/main/java/org/carewebframework/web/annotation/ComponentScanner.java
+++ org.carewebframework.web.core/src/main/java/org/carewebframework/web/annotation/ComponentScanner.java
@@ -7,15 +7,15 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * 
+ *
  * This Source Code Form is also subject to the terms of the Health-Related
  * Additional Disclaimer of Warranty and Limitation of Liability available at
  *
@@ -51,7 +51,7 @@ public class ComponentScanner extends AbstractClassScanner<BaseComponent, Compon
     /**
      * Creates and registers a component definition for a class by scanning the class and its
      * superclasses for method annotations.
-     * 
+     *
      * @param clazz Class to scan.
      */
     @Override
@@ -70,13 +70,18 @@ public class ComponentScanner extends AbstractClassScanner<BaseComponent, Compon
     /**
      * Scans a class for method annotations, adding them to the component definition as they are
      * found.
-     * 
+     *
      * @param def Component definition for the class.
      * @param clazz The class to be scanned.
      */
     private void scanMethods(ComponentDefinition def, Class<?> clazz) {
         for (Method method : clazz.getDeclaredMethods()) {
             method.setAccessible(true);
+            
+            if (method.isSynthetic() || method.isBridge()) {
+                continue;
+            }
+            
             PropertySetter setter = method.getAnnotation(PropertySetter.class);
             
             if (setter != null) {
